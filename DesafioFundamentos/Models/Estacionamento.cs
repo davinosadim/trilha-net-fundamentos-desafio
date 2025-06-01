0126a0d191b6bfeaa6b8564ebf7d606dd298d1df
Davino Sadim <davinomoises@outlook.com>
2025-06-01 11:41:43 -0400

    Método ÁdicionarVeiculo() resolvido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
+            
         }
 
         public void RemoverVeiculo()
@@ -35,7 +37,7 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
+                // int horas = 0;
                 decimal valorTotal = 0; 
 
                 // TODO: Remover a placa digitada da lista de veículos
